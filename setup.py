bec3a7193051108152ebf81a42d08bd6501343b9
Hong-She Liang <starofrainnight@gmail.com>
2017-08-17 15:20:38 +0800

    Fixed failed to install by setup.py

--- setup.py
+++ setup.py
@@ -44,11 +44,8 @@ class my_install_scripts(install_scripts):
 
 package_name = 'rabird.qt'
 
-# Convert source to v2.x if we are using python 2.x.
-source_dir = rabird.core.distutils.preprocess_source()
-
 # Exclude the original source package, only accept the preprocessed package!
-our_packages = find_packages(where=source_dir)
+packages = find_packages()
 
 our_requires = [
 ]
@@ -69,11 +66,10 @@ setup(
     long_description=long_description,
     license="Apache Software License",
     classifiers=[
-                "Development Status :: 3 - Alpha",
+            "Development Status :: 3 - Alpha",
             "Intended Audience :: Developers",
             'License :: OSI Approved :: Apache Software License',
             "Operating System :: OS Independent",
-            "Programming Language :: Python :: 2",
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: Implementation :: CPython",
             "Framework :: Rabird",
@@ -81,14 +77,13 @@ setup(
             "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     install_requires=our_requires,
-    package_dir={'': source_dir},
-    packages=our_packages,
+    # package_dir={'': source_dir},
+    packages=packages,
     namespace_packages=[package_name.split(".")[0]],
 
     # FIXME: I don't know why we can't use entry_points to install our script,
     # seems that problem related to "package_dir" .
-    scripts=[os.path.join(source_dir, 'scripts',
-                          'rbpyside-i18n-update.py')],
+    scripts=['./scripts/rbpyside-i18n-update.py'],
     cmdclass={'install_scripts': my_install_scripts},
 
     # If we don't set the zip_safe to False, pip can't find us.
