e7b68adae2aefe2767b055eeae293bf90315e0e6
Hong-She Liang <starofrainnight@gmail.com>
2017-08-17 15:38:34 +0800

    Include all package data
    Aligned arguments of setup()

--- setup.py
+++ setup.py
@@ -15,15 +15,19 @@ from distutils.command.install_scripts import install_scripts
 
 package_name = 'rabird.qt'
 
-install_requires = [
-    'qtpy>=1.3.0',
-]
-
 long_description = (
     open('README.rst', 'r').read()
     + '\n' +
     open('CHANGES.rst', 'r').read()
 )
+
+install_requires = [
+    'qtpy>=1.3.0',
+]
+
+tests_require = [
+    # TODO: put package test requirements here
+]
 
 setup(
     name=package_name,
@@ -31,25 +35,29 @@ setup(
     author='Hong-She Liang',
     author_email='starofrainnight@gmail.com',
     url='https://github.com/starofrainnight/%s' % package_name,
-    description='An extension library for Qt library',
-    long_description=long_description,
-    license="Apache Software License",
-    classifiers=[
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Developers",
-            'License :: OSI Approved :: Apache Software License',
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 3",
-            "Topic :: Software Development :: Libraries",
-    ],
-    install_requires=install_requires,
     packages=find_packages(),
     namespace_packages=[package_name.split(".")[0]],
+    description='An extension library for Qt library',
+    long_description=long_description,
     entry_points={
         'console_scripts': [
             'rbqt-i18n-update=rabird.qt.commands:i18n_update'
         ]
     },
+    include_package_data=True,
+    install_requires=install_requires,
+    license="Apache Software License",
     # If we don't set the zip_safe to False, pip can't find us.
     zip_safe=False,
+    keywords=package_name,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Apache Software License',
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+    ],
+    test_suite='tests',
+    tests_require=tests_require
 )
