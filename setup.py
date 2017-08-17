dd713b125559b8b39a0529388fea90d3764a33fa
Hong-She Liang <starofrainnight@gmail.com>
2017-08-17 17:24:29 +0800

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ tests_require = [
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.1.1',
     author='Hong-She Liang',
     author_email='starofrainnight@gmail.com',
     url='https://github.com/starofrainnight/%s' % package_name,
