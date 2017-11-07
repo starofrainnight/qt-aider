c28677fa54f240eca571752fea39922fec28c4a1
Hong-She Liang <starofrainnight@gmail.com>
2017-11-07 14:47:46 +0800

    Added InitMixin for QCoreApplication and QApplication
    It fixs problems they works with python and embed eventlet framework

---
+++ rabird/qt/application.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import signal
+import logging
+from . import embed
+
+
+class InitMixin(object):
+    """
+    This mixin use to fix QCoreApplication for python:
+
+    1. Fixed can't break program by Ctrl+C
+    2. Fixed installTranslator() won't take effect until we keep the
+       translator's reference manually.
+    3. Integrated with eventlet framework, so we could use coroutines!
+    """
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+        # Embed eventlet
+        embed.embedEventlet(self)
+
+        # FIXME: Just use to keep translators references for avoid they be
+        # deleted at runtime.
+        self._translators = []
+
+    def installTranslator(self, translator):
+        """
+        Override installTranslator() of QCoreApplication, so the translator's
+        references won't missing.
+        """
+        self._translators.append(translator)
+        return super().installTranslator(translator)
+
+    def removeTranslator(self, translator):
+        """
+        Override removeTranslator() of QCoreApplication
+        """
+
+        self._translators.remove(translator)
+        return super().removeTranslator(translator)
