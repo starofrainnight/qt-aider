2e70d05d2c4e9ab2554c8a372cffd5065fbac9d3
Hong-She Liang <starofrainnight@gmail.com>
2019-01-09 15:00:47 +0800

    Fixed open() does not have 'encoding' argument in older pythons

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@
 
 """The setup script."""
 
+import io
 from setuptools import setup, find_packages
 
-with open("README.rst", encoding="utf-8") as readme_file, open(
+with io.open("README.rst", encoding="utf-8") as readme_file, io.open(
     "HISTORY.rst", encoding="utf-8"
 ) as history_file:
     long_description = readme_file.read() + "\n\n" + history_file.read()
